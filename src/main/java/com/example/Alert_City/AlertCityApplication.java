3a2693ad7808d21ae24ae054d46fb4fb5f37e1f9
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2025-10-26 03:31:02 -0300

    chore: initialize Alert City project

---
+++ src/main/java/com/example/Alert_City/AlertCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Alert_City;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Alert_City/AlertCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Alert_City;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
