236b54be3bdf2134015807bea568021c602b1c9e
Hallason Matias <hms3@cin.ufpe.com>
2025-11-11 11:43:43 -0300

    feat: add the function of login with secure auth

--- src/main/java/com/example/Alert_City/controller/AuthController.java
+++ src/main/java/com/example/Alert_City/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.Alert_City.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Alert_City.model.UserModel;
+import com.example.Alert_City.security.JwtUtil;
 import com.example.Alert_City.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,5 +26,15 @@ public class AuthController {
     public ResponseEntity<?> registerUser(@RequestBody Map<String, String> request) {
         UserModel user = userService.registerUser(request.get("name"), request.get("password"));
         return ResponseEntity.ok(user);
-    }  
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody Map<String, String> request) {
+        Optional<UserModel> user = userService.findByName(request.get("name"));
+        if (user.isPresent() && user.get().getPassword().equals(request.get("password"))) {
+            String token = JwtUtil.generateToken(user.get().getName());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("Invalid Credentials");
+    }
 }
